e1b472077074c02c8041396886f5e3ed61af3a0f
Francisco Guzmán <paco.mgh@gmail.com>
2021-01-16 09:55:23 -0600

    add images to avatar page

--- flutter/componentes/lib/src/pages/avatar_page.dart
+++ flutter/componentes/lib/src/pages/avatar_page.dart
@@ -8,7 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.plymouth.edu/theclock/wp-content/uploads/sites/183/2019/03/stan-lee.png'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),            
+              backgroundColor: Colors.brown,
+            ),
+          ),          
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://www.elsoldeacapulco.com.mx/incoming/wpgvi0-villanos-creados-por-stan-lee.jpg/ALTERNATES/LANDSCAPE_768/Villanos%20creados%20por%20Stan%20lee.jpg'),
+          placeholder: AssetImage('assets/jat-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),      
     );
   }
 }
