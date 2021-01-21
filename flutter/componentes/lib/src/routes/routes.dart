044613fda683dd9a96b43d24b7415125ef827e6e
Francisco Guzmán <paco.mgh@gmail.com>
2021-01-21 09:58:30 -0600

    add slider and checboxes

--- flutter/componentes/lib/src/routes/routes.dart
+++ flutter/componentes/lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slide_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 
@@ -18,6 +19,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'card'   : (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(), 
     'inputs': (BuildContext context) => InputPage(), 
+    'slider': (BuildContext context) => SliderPage(), 
   };
 
 }
