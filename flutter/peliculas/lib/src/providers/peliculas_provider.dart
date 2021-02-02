aa3aa66e9ea72aec43b6b35e083640ad6b88674a
Francisco Guzmán <paco.mgh@gmail.com>
2021-02-02 10:03:09 -0600

    add movies to infinite horizontal scroll

--- flutter/peliculas/lib/src/providers/peliculas_provider.dart
+++ flutter/peliculas/lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+import 'dart:async';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
 
@@ -8,6 +9,26 @@ class PeliculasProvider{
   String _apiKey    = 'e025d77ce91c7b698e4af70d16ca4a4f';
   String _url       = 'api.themoviedb.org';
   String _language  = 'es-ES';
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = [];
+
+  //creamos stream para las peliculas populares
+  //le agregamos el broadcasts para que varias instancias puedan escuchar el stream
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  //get para insertar informacion
+  //apunta al sink del stream controller para agregar peliculas
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+  //get para escuchar lo que emite el stream
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+
+  //siempre debemos cerrar los streams cuando no se usen y no se creen multiples streams
+  void disposeStreams(){
+    //preguntamos si tiene algun valor, si lo tiene se cierra el stream
+    _popularesStreamController?.close();
+  }
 
   //metodo para retornar la lista de peliculas, sin importar la categoria que seleccionemos
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async{
@@ -41,24 +62,23 @@ class PeliculasProvider{
   //llamaremos las peliculas mas recientes
   Future<List<Pelicula>> getPopulares() async{
 
-    //genera el url de manera sencilla, incluye https://
+    _popularesPage++;
 
+    //genera el url de manera sencilla, incluye https://
     final url = Uri.https(_url, '3/movie/popular', {
       //enviamos los parametros del mapa
       'api_key': _apiKey,
-      'language': _language
+      'language': _language,
+      'page': _popularesPage.toString()
     });
-    //este fragmento sustituye al comentado abajo
-    return await _procesarRespuesta(url);
-    //retorna toda la respuesta http, para almacenar el resultado y no el future usamos await
-    //final resp = await http.get(url);
-    //decodificamos la data de la respuesta y lo tranformamos en un mapa
-    //final decodedData = json.decode(resp.body);
-    //barre cada uno de los resultados y genera la pelicula de acuerdo al modelo
-    //final peliculas = new Peliculas.fromJsonList(decodedData['results']);
-    //print(decodedData['results']);
-    //retornamos las peliculas ya mapeadas
-    //return peliculas.items;
+
+    final resp = await _procesarRespuesta(url);
+    //mandamos todo lo que contenga la respuesta
+    _populares.addAll(resp);
+    //lo colocamos en el inicio con sink
+    popularesSink(_populares);
+
+    return resp;
   }
 
 
--- flutter/peliculas/lib/src/widgets/movie_horizontal.dart
+++ flutter/peliculas/lib/src/widgets/movie_horizontal.dart
@@ -5,23 +5,34 @@ class MovieHorizontal extends StatelessWidget {
   //const MovieHorizontal({Key key}) : super(key: key);
 
   final List<Pelicula> peliculas;
+  final Function siguientePagina;
 
-  MovieHorizontal({@required this.peliculas});
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+
+  final _pageController = new PageController(
+    initialPage: 1,//hace que sea fluido
+    viewportFraction: 0.3//cuantas tarjetas se muestran en el viewport
+  );
 
   @override
   Widget build(BuildContext context) {
     //obtenemos el tamaño de la pantalla
     final _screenSize = MediaQuery.of(context).size;
+    //obtenemos la posicion en pixeles del pagecontroller
+    _pageController.addListener(() { 
+      //verificamos que el scroll sea menor que el scroll maximo
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200){
+        siguientePagina();
+      }
+    });
+
     //container se usa para manejar la separacion
     return Container(
       height: _screenSize.height*0.2,//obtenemos el 20%
       //sirve para deslizar widgets o paginas
       child: PageView(
         pageSnapping: false,//da mejor flujo
-        controller: PageController(
-          initialPage: 1,//hace que sea fluido
-          viewportFraction: 0.3//cuantas tarjetas se muestran en el viewport
-        ),
+        controller: _pageController,
         children:_tarjetas(context),
       ),
     );
