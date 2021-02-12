77798f041e908e643cb43a38518b56ac17d9b59e
Francisco Guzmán <paco.mgh@gmail.com>
2021-02-12 09:26:24 -0600

    add movies suggestions

--- flutter/peliculas/lib/src/search/search_delegate.dart
+++ flutter/peliculas/lib/src/search/search_delegate.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate{
+
+  String seleccion="";
+
+  final peliculas = [
+    'Spiderman',
+    'Shazam',
+    'Avengers',
+    'Capitan America',
+    'Iron Man',
+    'Iron Man 2',
+    'Iron Man 3',
+  ];
+
+  final peliculasRecientes = [
+    'Spiderman',
+    'Capitan America'
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
       // Las acciones de nuestro appbar
@@ -33,13 +51,39 @@ class DataSearch extends SearchDelegate{
     @override
     Widget buildResults(BuildContext context) {
       // Crea los resultados que vamos a buscar
-      return Container();
+      return Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.blueAccent,
+          child: Text(seleccion),
+        ),
+      );
     }
   
     @override
     Widget buildSuggestions(BuildContext context) {
+
+      final listaSugerida = ( query.isEmpty )//si el query esta vacio, enviamos peliculas recientes
+                            ? peliculasRecientes
+                            : peliculas.where(//sino seleccionamos la pelicula en donde inicie el nombre de
+                              (p) => p.toLowerCase().startsWith(query.toLowerCase())//la pelicula con el query escrito
+                            ).toList();//lo regresamos en una lista
+
     // Son las sugerenias que aparecen al escribir
-    return Container();
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context,i){
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: (){
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 
 }
