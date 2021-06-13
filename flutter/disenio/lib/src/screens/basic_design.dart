61793429b2fad3414b6a18677ce02e6f5e10f8e5
Paco <paco.mgh@gmail.com>
2021-06-13 14:53:39 -0500

    add button and paragraph section

--- flutter/disenio/lib/src/screens/basic_design.dart
+++ flutter/disenio/lib/src/screens/basic_design.dart
@@ -12,7 +12,14 @@ class BasicDesignScreen extends StatelessWidget {
           Image( image: AssetImage('assets/landscape.jpg'),),
 
           //title widget
-          Title()
+          Title(),
+          //button section
+          ButtonSection(),
+          //paragraph section
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text("Dolor non eiusmod labore Lorem do adipisicing veniam. Dolor sit aute pariatur labore proident consectetur anim fugiat nisi. Ullamco anim et amet pariatur ad ex enim magna. Ea veniam ea consectetur fugiat mollit occaecat voluptate aliqua elit est nisi. Cupidatat ullamco aute ipsum elit officia aliqua enim."),
+          ),
         ],
      ),
    );
@@ -43,6 +50,51 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  } 
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric( vertical: 20, horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomBouton(icon: Icons.call, text: "Calls",),
+          CustomBouton(icon: Icons.alt_route, text: "Route",),
+          CustomBouton(icon: Icons.share, text: "Share",),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomBouton extends StatelessWidget {
+  //por si no sabemos que tipo de icono es
+  final IconData icon;
+  final String text;
+
+  const CustomBouton({
+    Key? key, 
+    //usamos required para indicar que es obligatorio dart v2.12+
+    required this.icon, 
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue, size: 30),
+        Container(height: 10,),
+        Text(this.text, style:TextStyle(color: Colors.blue),)
+      ],
     );
   }
 }
